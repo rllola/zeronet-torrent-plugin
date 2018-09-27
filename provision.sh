c912612b5512a7acafcae64a80720bb789711cc9
rllola <rllola80@gmail.com>
2018-09-27 18:28:06 +0200

    Adding lib in li/

--- provision.sh
+++ provision.sh
@@ -2,7 +2,7 @@
 
 echo "===== INSTALL DEPENDENCIES ====="
 sudo apt update
-sudo apt install -y python-pip msgpack-python python-gevent libboost-all-dev git
+sudo apt install -y python-pip msgpack-python python-gevent git
 # apt install -y autoconf libtool build-essential libboost-all-dev libssl-dev
 # This line broke pip so lets not use it for now
 #pip install --upgrade --user pip
@@ -10,15 +10,15 @@ sudo pip install conan
 
 echo "===== UPDATE CMAKE (for trusty) ====="
 sudo apt purge cmake
-wget https://cmake.org/files/v3.12/cmake-3.12.0-Linux-x86_64.tar.gz
-tar -xvf cmake-3.12.0-Linux-x86_64.tar.gz
-cd cmake-3.12.0-Linux-x86_64
+wget https://cmake.org/files/v3.12/cmake-3.12.1-Linux-x86_64.tar.gz
+tar -xvf cmake-3.12.1-Linux-x86_64.tar.gz
+cd cmake-3.12.1-Linux-x86_64
 sudo cp -r bin /usr/
 sudo cp -r share /usr/
 sudo cp -r doc /usr/share/
 sudo cp -r man /usr/share/
 cd ..
-sudo rm -r cmake-3.12.0-Linux-x86_64
+sudo rm -r cmake-3.12.1-Linux-x86_64
 
 echo "===== INSTALL ZERONET ====="
 wget https://github.com/HelloZeroNet/ZeroNet/archive/master.tar.gz
@@ -26,6 +26,6 @@ sudo tar xvpfz master.tar.gz
 
 echo "===== INSTALL LIBTORRENT & LIBTORRENT PYTHON BINDINGS ====="
 conan remote add libtorrent https://api.bintray.com/conan/rllola80/Libtorrent
-cd /home/ubuntu/ZeroNet-master/plugins/Torrent
+cd /home/vagrant/ZeroNet-master/plugins/Torrent
 conan install --build=missing .
 conan build .
