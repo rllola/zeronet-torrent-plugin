82441f52f7f077ab93e8be09b77f0eb252fd4c81
rllola <rllola80@gmail.com>
2018-10-09 00:26:17 +0200

    Working ?

--- conanfile.py
+++ conanfile.py
@@ -165,8 +165,8 @@ class LibtorrentPythonConan(ConanFile):
         print "Looking for Python libraries"
         library_dirs, include_dir = FindPythonLibraries()
         print "We got it"
-        #pythonpaths = "-DPYTHON_INCLUDE_DIR=" + include_dir + " -DPYTHON_LIBRARY=" + library_dirs
-        pythonpaths = "-DPYTHON_INCLUDE_DIR=/usr/include/python2.7 -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython2.7.so"
+        pythonpaths = "-DPYTHON_INCLUDE_DIR=" + include_dir + " -DPYTHON_LIBRARY=" + library_dirs
+        #pythonpaths = "-DPYTHON_INCLUDE_DIR=/usr/include/python2.7 -DPYTHON_LIBRARY=/usr/lib/x86_64-linux-gnu/libpython2.7.so"
         print pythonpaths
         self.run('cmake src %s %s -DEXAMPLE_PYTHON_VERSION=%s' % (cmake.command_line, pythonpaths, self.options.python_version))
         self.run("cmake --build . %s" % cmake.build_config)
