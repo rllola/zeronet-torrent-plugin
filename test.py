5260797835506e8c22670a1ce7e8a61b72947408
rllola <rllola80@gmail.com>
2018-10-22 22:11:35 +0200

    test session after build

--- test.py
+++ test.py
@@ -6,6 +6,8 @@ class TestBuild(unittest.TestCase):
     def test_build(self):
         self.assertEqual(libtorrent.version, '1.1.10.0')
 
+    def test_session(self):
+        session = libtorrent.session({'listen_interfaces':'0.0.0.0:6881', 'alert_mask': libtorrent.alert.category_t.progress_notification})
 
 if __name__ == '__main__':
     unittest.main()
