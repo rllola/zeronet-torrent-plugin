c8e9573a6103f90ba07154e706f8ab3e87691691
Lola Dam <rllola80@gmail.com>
2021-08-01 13:17:55 +0200

    unzip libtorrent module (#33)

--- __init__.py
+++ __init__.py
@@ -1,5 +1,17 @@
 import sys
 import os
+import zipfile
+
+if sys.platform == 'win32':
+  libtorrent_file = 'libtorrent.pyd'
+else:
+  libtorrent_file = 'libtorrent.so'
+
+ 
+# if libtorrent lib not present look for it
+if not os.path.exists(os.path.join(os.path.dirname(__file__),'libtorrent', libtorrent_file)):
+  with zipfile.ZipFile('libtorrent-{}.zip'.format(sys.platform),'r') as zip_ref:
+    zip_ref.extractall('libtorrent')
 
 # Need to add libtorrent lib
 sys.path.append(os.path.dirname(__file__))
